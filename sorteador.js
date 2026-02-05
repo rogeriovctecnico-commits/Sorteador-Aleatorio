b5438c68407be747dcbb2f5a0f490edf0ecc2799
rogeriovctecnico-commits <rogeriovctecnico@gmail.com>
2026-02-05 19:15:39 -0300

    projeto de sorteio onde aprendi alterar valores na pagina html atraves de javascripts, está funcional

--- sorteador.js
+++ sorteador.js
@@ -1,1 +1,11 @@
+function GenerateNumber() {
+    const min = Math.ceil(document.querySelector('.min').value);
+    const max = Math.floor(document.querySelector('.max').value);
+    console.log(min, max);
 
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Seleciona o primeiro elemento com a classe 'saida-valor'
+    const resultado = document.getElementsByClassName('saida-valor')[0];
+    resultado.textContent = result;
+}
